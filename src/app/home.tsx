d5a56e295c8a7e9aff9036ba87ad100cbcd05506
Santosh Yadav <ymanoutube@gmail.com>
2024-09-27 15:08:18 +0545

    Routing Setup

---
+++ src/app/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto flex bg-green-200 items-center justify-center">
+      Home
+    </div>
+  );
+};
+
+export default Home;
