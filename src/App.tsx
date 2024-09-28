03504a1db86c198bd3af145729a3ade234bccab7
Santosh Yadav <ymanoutube@gmail.com>
2024-09-28 11:33:03 +0545

    integrated authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./app/home";
+import { Routes, Route } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import Navbar from "./components/blocks/navbar";
+
 import Footer from "./components/blocks/footer";
+import Home from "./app/home";
+import logo from "./assets/logo2.png";
 
 function App() {
   return (
-    <div className="flex flex-col justify-between min-h-screen">
-      <div>
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-         
-        </Routes>
-      </div>
+    <div className="flex flex-col justify-between min-h-screen ">
+      <Navbar />
+      <SignedOut>
+        <div className="flex justify-center flex-col items-center        ">
+          <img className="w-[20%] " src={logo} alt="" />
+          <h1 className="text-4xl font-bold p-3">Welcome to Algoo</h1>
+          <p className="text-lg text-center p-3 md:w-[80ch] text-gray-500">
+            Algoo is a simple calculator that helps you calculate your daily
+            needs. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis nihil, ullam aliquid error eos totam suscipit iste enim
+            consequuntur adipisci?
+          </p>
+          <SignInButton>
+            <button className="bg-green-500 text-white font-bold py-2 px-4 rounded">
+              Sign In
+            </button>
+          </SignInButton>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div>
+          <div className="m-3">
+            <UserButton />
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </SignedIn>
       <Footer />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <App />
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 );
